edaf5fd04d37e78f80162721d97b07717b0f3c41
Marc Hu <marc.hu@etu.upmc.fr>
2018-02-28 08:48:13 +0100

    first commit

---
+++ pySpriteWorld-forStudents/UltimateTicTacToe.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+# Nicolas, 2015-11-18
+
+from __future__ import absolute_import, print_function, unicode_literals
+from gameclass import Game,check_init_game_done
+from spritebuilder import SpriteBuilder
+from players import Player
+from sprite import MovingSprite
+from ontology import Ontology
+from itertools import chain
+import pygame
+import glo
+
+import random 
+import numpy as np
+import sys
+sys.path.insert(0, '../1-rechercheHeuristique/')
+from  grid2D import ProblemeGrid2D
+from probleme import *
+
+def getPath(node,path):
+    if node != None:
+        getPath(node.pere,path)
+        path.append(node.etat)
+
+    
+# ---- ---- ---- ---- ---- ----
+# ---- Main                ----
+# ---- ---- ---- ---- ---- ----
+
+game = Game()
+
+def init(_boardname=None):
+    global player,game
+    # pathfindingWorld_MultiPlayer4
+    name = _boardname if _boardname is not None else 'tictactoe'
+    game = Game('Cartes/' + name + '.json', SpriteBuilder)
+    game.O = Ontology(True, 'SpriteSheet-32x32/tiny_spritesheet_ontology.csv')
+    game.populate_sprite_names(game.O)
+    game.fps = 5  # frames per second
+    game.mainiteration()
+    game.mask.allow_overlaping_players = True
+    #player = game.player
+    
+def main():
+
+    #for arg in sys.argv:
+    iterations = 50 # default
+    if len(sys.argv) == 2:
+        iterations = int(sys.argv[1])
+    print ("Iterations: ")
+    print (iterations)
+
+    init()
+    
+    
+    
+
+    
+    #-------------------------------
+    # Initialisation
+    #-------------------------------
+       
+    players = [o for o in game.layers['joueur']]
+    nbPlayers = len(players)
+    #score = [0]*nbPlayers
+    fioles = {} # dictionnaire (x,y)->couleur pour les fioles
+    
+    
+    # on localise tous les états initiaux (loc du joueur)
+    initStates = [o.get_rowcol() for o in game.layers['joueur']]
+    print ("Init states:", initStates)
+    
+    
+    # on localise tous les objets ramassables
+    #goalStates = [o.get_rowcol() for o in game.layers['ramassable']]
+    #print ("Goal states:", goalStates)
+        
+    # on localise tous les murs
+    wallStates = [w.get_rowcol() for w in game.layers['obstacle']]
+    tictactoeStates = [(x,y) for x in range(3,16) for y in range(3,16)]
+    print ("Wall states:", wallStates)
+    #-------------------------------
+    # Placement aleatoire des fioles de couleur 
+    #-------------------------------
+    
+    for o in game.layers['ramassable']: # on considère chaque fiole
+        
+        #on détermine la couleur
+    
+        if o.tileid == (19,0): # tileid donne la coordonnee dans la fiche de sprites
+            couleur = 'r'
+        elif o.tileid == (19,1):
+            couleur = 'j'
+        else:
+            couleur = 'b'
+
+        # et on met la fiole qqpart au hasard
+
+        x = random.randint(1,19)
+        y = random.randint(1,19)
+
+        while (x,y) in tictactoeStates or (x,y) in wallStates or (x,y) in fioles: # ... mais pas sur un mur
+            x = random.randint(1,19)
+            y = random.randint(1,19)
+        o.set_rowcol(x,y)
+        # on ajoute cette fiole 
+        fioles[(x,y)]=couleur
+
+        game.layers['ramassable'].add(o)
+        game.mainiteration()                
+
+    print("Les ", len(fioles), " fioles ont été placées aux endroits suivants: \n", fioles)
+
+
+    
+    
+    #-------------------------------
+    # Boucle principale de déplacements 
+    #-------------------------------
+    
+        
+    # bon ici on fait juste plusieurs random walker pour exemple...
+    
+    posPlayers = initStates
+    grid = np.ones((20, 20), dtype=bool)
+    for c in wallStates:
+        grid[c[0]][c[1]] = False
+    
+    ramassable = [w.get_rowcol() for w in game.layers['ramassable']]
+    all_path = []
+    for i in range(nbPlayers):
+        pos = posPlayers[i]
+        target = ramassable[i]
+        p2 = ProblemeGrid2D(pos,target,grid,'manhattan')
+        node = astar(p2,True,False);
+        path = []
+        getPath(node,path)
+        all_path.append(path)
+    
+
+    for i in range(nbPlayers):
+        
+        for step in all_path[i]: # on fait bouger chaque joueur séquentiellement
+            row,col = posPlayers[i]
+
+            x_inc,y_inc = step
+            next_row = row+x_inc
+            next_col = col+y_inc
+            # and ((next_row,next_col) not in posPlayers)
+            if ((next_row,next_col) not in wallStates) and next_row>=0 and next_row<=19 and next_col>=0 and next_col<=19:
+                players[j].set_rowcol(next_row,next_col)
+                print ("pos :", i, next_row,next_col)
+                game.mainiteration()
+    
+                col=next_col
+                row=next_row
+                posPlayers[i]=(row,col)
+            
+      
+        
+            
+            # si on trouve un objet on le ramasse
+            if (row,col) in fioles:
+                o = players[i].ramasse(game.layers)
+                game.mainiteration()
+                print ("Objet de couleur ", fioles[(row,col)], " trouvé par le joueur ", i)
+                fioles.pop((row,col))   # on enlève cette fiole de la liste
+                
+                 
+                
+                #break
+            
+    
+    print (fioles)
+    pygame.quit()
+      
+    
+   
+
+if __name__ == '__main__':
+    main()
+    
+
+
